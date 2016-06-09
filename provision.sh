608cdfb949f24b48e06c150d8c0a4b87b6f6a7ff
Pär Eriksson <pe222gq@student.lnu.se>
2016-06-09 21:44:46 +0200

    Adding development environment.

---
+++ provision.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Add repository
+sudo apt-add-repository ppa:brightbox/ruby-ng-experimental
+
+# Update and install
+sudo apt-get update
+sudo apt-get install -y build-essential git curl language-pack-sv
+sudo apt-get install -y ruby2.0 ruby2.0-dev ruby2.0-doc
+sudo gem install jekyll github-pages
